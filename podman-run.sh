b8f6658fb72d607be1aa09903940d0365699980f
theinfinite3 <3228-theinfinite3@users.noreply.gitlab.com>
2025-10-19 10:31:56 -0400

    Add some cusomtizations to run with VNC

--- podman-run.sh
+++ podman-run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-podman run -d --name coder-server-dev1 \
+podman run -d --name coder-server-vnc \
 	    --cpus 2 \
 	    --shm-size '1024m' \
 	    --secret .ssh_id_ed25519,mode=600,target=/config/.ssh/id_ed25519 \
@@ -10,7 +10,7 @@ podman run -d --name coder-server-dev1 \
 	    -e PASSWORD=password \
 	    -e SUDO_PASSWORD=password \
 	    -e DEFAULT_WORKSPACE=/config/workspace \
-	    -e PWA_APPNAME=code-server-dev1 \
+	    -e PWA_APPNAME=code-server-vnc \
 	    -p 127.0.0.1:8443:8443 \
 	    -p 127.0.0.1:6080:6080 \
 	    -v $HOME/code/code-server-workspace:/config/workspace \
